d5383387e20fcbc0071ca7cf68d98df4c3e09195
Max <4myself.max@gmail.com>
2025-01-30 11:53:22 +0100

    Init commit

---
+++ src/main/java/com/example/microstream/dataaggregator/DataaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.microstream.dataaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microstream/dataaggregator/DataaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microstream.dataaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
